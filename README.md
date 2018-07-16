de46242ee478ba106f9113291f0af278325294f8
borys15 <borys.hayduchyk@true-ag.com>
2018-07-16 15:53:20 +0300

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+LangBehavior
+============
+LangBehavior
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist boryshaiduchuk/yii2-lang-behavior "*"
+```
+
+or add
+
+```
+"boryshaiduchuk/yii2-lang-behavior": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \boryshaiduchuk\langbehavior\AutoloadExample::widget(); ?>```
---
+++ src/LangBehavior.php
@@ -0,0 +1,113 @@
+<?php
+namespace boryshaiduchuk\langbehavior;
+
+use yii\db\ActiveRecord;
+
+/**
+ * Behavior translate columns from main model and save in auxiliary
+ */
+class LangBehavior extends \yii\base\Behavior
+{
+    public $t;
+    public $fk;
+    public $l;
+
+    /**
+     * @inheritdoc
+     */
+    public function events()
+    {
+        return[
+            ActiveRecord::EVENT_AFTER_INSERT => 'afterSave',
+            ActiveRecord::EVENT_BEFORE_VALIDATE => 'populateAttributes',
+            ActiveRecord::EVENT_AFTER_UPDATE => 'afterSave',
+            ActiveRecord::EVENT_AFTER_FIND => 'afterFind',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function afterSave()
+    {
+        $this->saveTranslation();
+
+        return true;
+    }
+
+    /**
+     * Assign data to Model from parent model
+     */
+    public function populateAttributes()
+    {
+        $this->t->setAttributes($this->owner->attributes, false);
+        $this->t->lang_id = $this->l;
+
+        foreach ($this->t->attributes as $attr => $val) {
+            if ($this->owner->hasProperty($attr) || $this->owner->hasAttribute($attr)) {
+                $this->t->{$attr} = $this->owner->{$attr};
+            }
+        }
+        if (!$this->t->validate()) {
+            foreach ($this->t->errors as $k => $v) {
+                $this->owner->addError($k, $v[0]);
+            }
+        }
+    }
+
+    /**
+     * Save translate model
+     */
+    public function saveTranslation()
+    {
+        $this->t->{$this->fk} = $this->owner->primaryKey;
+        //$this->t->{$this->fk}
+        if ($this->t->validate()) {
+            $this->t->save();
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function afterFind()
+    {
+        $this->initTranslation();
+    }
+
+    /**
+     * @param boolean $anyTranslation
+     */
+    public function initTranslation($anyTranslation = false)
+    {
+        if ($model = $this->getTranslates($anyTranslation)->one()) {
+            $this->t = $model;
+            if ($this->t->isNewRecord) {
+                return $model;
+            }
+            foreach ($this->t->attributes as $attr => $val) {
+                if ($attr == 'order') {
+                    continue;
+                }
+                if ($this->owner->hasProperty($attr)) {
+                    $this->owner->{$attr} = $val;
+                }
+            }
+        }
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTranslates($anyTranslation = false)
+    {
+        $condition = [$this->fk => $this->owner->primaryKey];
+        if (!$anyTranslation)
+            $condition['lang_id'] = $this->l;
+        $this->t->{$this->fk} = $this->owner->primaryKey;
+        $q = $this->t->find();
+        $q->where($condition);
+        return $q;
+    }
+
+}
